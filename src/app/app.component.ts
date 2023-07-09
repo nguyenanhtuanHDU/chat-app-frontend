d2a7e84f4c3ea3451598d65dfed1d1d221403b71
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-07-09 20:59:09 +0700

    base socket send & get message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // private socket: SocketIOClient.Socket;
+
+  private socket = io('http://localhost:8000');
+  messages: string[] = [];
+  ngOnInit() {
+    this.socket.on('connect', () => {
+      console.log('Connected to socket server');
+    });
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from socket server');
+    });
+    this.socket.on('message', (message: string) => {
+      this.messages.push(message);
+      console.log('Received message:', message);
+    });
+  }
+
   title = 'chat-app-frontend';
+  message!: string;
+
+  sendMessage() {
+    this.socket.emit('message', this.message);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = {
+  url: 'http://localhost:8000',
+  options: {
+    withCredentials: true,
+    transports: ['websocket', 'polling'],
+    autoConnect: false,
+  },
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, SocketIoModule.forRoot(config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
